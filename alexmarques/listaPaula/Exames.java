49997fbfb64c116f9eca8f48abf5180d93b3b89b
AlexMarques <alex@recriarsistemas.com.br>
2025-02-08 21:42:38 -0300

    first commit

---
+++ alexmarques/listaPaula/Exames.java
@@ -0,0 +1,5 @@
+package alexmarques.listaPaula;
+
+public class Exames {
+
+}
---
+++ alexmarques/listaPaula/PaulaMain.java
@@ -0,0 +1,12 @@
+package alexmarques.listaPaula;
+
+import java.util.Scanner;
+import java.io.RandomAccessFile;
+
+public class PaulaMain {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+
+    }
+    
+}
